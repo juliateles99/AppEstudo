7af508a3e8edabf1dc1c6c6684a00e217a783f37
Marcelly Luise <Marcelly.Godinho@AB-Inbev.com>
2023-04-13 18:18:23 -0300

    Some experiments trying to fix image roundness

--- UITableView/Controller/ItemDetailViewController.swift
+++ UITableView/Controller/ItemDetailViewController.swift
@@ -11,9 +11,11 @@ class ItemDetailViewController: UIViewController {
     
     // MARK: - DataSource
     public var agent: Agent?
+    //
     
-    private var imageDetails: UIImageView = UIImageView(frame: .zero)
-    
+    // TODO: - Fix image loading
+    var imageDetails: UIImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+    //
     private let labelAgentsText: UILabel = {
         let lbAgentsDetails = UILabel()
         lbAgentsDetails.font = .systemFont(ofSize: 35, weight: .medium)
@@ -27,45 +29,48 @@ class ItemDetailViewController: UIViewController {
         lbAgentsDesciption.font = .systemFont(ofSize: 20, weight: .light)
         lbAgentsDesciption.numberOfLines = 0
         lbAgentsDesciption.textAlignment = .justified
-        
+            
         return lbAgentsDesciption
     }()
-    
-     private func configure() {
-         if let avatar = agent?.avatar, let agentImage = UIImage(named: avatar) {
-             imageDetails = UIImageView(image: agentImage)
-             imageDetails.contentMode = .scaleAspectFit
-         }
-         imageDetails.layoutSubviews()
-         
-         self.labelAgentsText.text = agent?.name
-         self.labelAgentsDescription.text = agent?.about
-    }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Detalhes do Agente"
-        self.setupUI()
+        
+        DispatchQueue.main.async {
+            self.setupUI()
+            self.configure()
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        configure()
     }
     
     private func setupUI() {
         
+        var width = 80.0
+        var height = 80.0
+        
+        if let avatar = agent?.avatar, let agentImage = UIImage(named: avatar) {
+            width = agentImage.size.width
+            height = agentImage.size.height
+        }
+        
         let stackView = UIStackView(frame: .zero)
+        
         stackView.addArrangedSubview(imageDetails)
         stackView.addArrangedSubview(labelAgentsText)
         stackView.addArrangedSubview(labelAgentsDescription)
         stackView.alignment = .center
         stackView.axis = .vertical
         stackView.distribution = .equalCentering
-
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        
         
         view.addSubview(stackView)
         
@@ -74,14 +79,36 @@ class ItemDetailViewController: UIViewController {
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
             
-            imageDetails.widthAnchor.constraint(equalToConstant: imageDetails.frame.size.width),
-           imageDetails.heightAnchor.constraint(equalToConstant: imageDetails.frame.size.height),
- ])
-        imageDetails.layoutIfNeeded()
-        imageDetails.layer.cornerRadius = imageDetails.frame.size.height / 2
-        print(imageDetails.frame.size.height / 2)
-        imageDetails.layer.masksToBounds = true
-        imageDetails.translatesAutoresizingMaskIntoConstraints = false
+            imageDetails.widthAnchor.constraint(equalToConstant: width),
+            imageDetails.heightAnchor.constraint(equalToConstant: height)
+        ])
+        
+        //        imageDetails.layoutIfNeeded()
+        //        imageDetails.layer.cornerRadius = imageDetails.frame.size.height / 2
+        //        print(imageDetails.frame.size.height / 2)
+        //        imageDetails.layer.masksToBounds = true
+        
+        //        imageDetails.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+    }
+    
+    private func configure() {
+        if let avatar = agent?.avatar, let agentImage = UIImage(named: avatar) {
+            
+            print("The current image is: \(agentImage)")
+            
+            imageDetails = UIImageView(image: UIImage(named: avatar, in: .main, compatibleWith: UITraitCollection.current))
+            //                imageDetails.image = agentImage
+            imageDetails.contentMode = .scaleAspectFit
+            
+        }
+        
+        //        imageDetails.layoutSubviews()
+        
+        labelAgentsText.text = agent?.name
+        labelAgentsDescription.text = agent?.about
     }
     
 }
